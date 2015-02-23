5acf943e8e8f54b442238e4ebd86a08282f795cb
krissss <462679766@qq.com>
2015-02-23 15:10:44 +0800

    添加数据库表缓存

--- config/db.php
+++ config/db.php
@@ -7,6 +7,7 @@ return [
     'username' => 'root',
     'password' => 'root',
     'charset' => 'utf8',
+    //添加数据库表缓存
     'enableSchemaCache' => true,
     'schemaCacheDuration' => 3600,
     'schemaCache' => 'cache',
