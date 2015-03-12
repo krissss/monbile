227493452b73fa60c932293f13cb6d670a973767
krissss <462679766@qq.com>
2015-03-12 10:14:58 +0800

    360使用急速模式

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -21,6 +21,8 @@ $user = Yii::$app->getSession()->get('user');
 <head>
     <meta charset="<?= Yii::$app->charset ?>"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?//360使用急速模式?>
+    <meta name="renderer" content="webkit">
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
